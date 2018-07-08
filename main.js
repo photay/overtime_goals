3c22a0e629d69a969ebdc6d32041f63b1613731c
Ayat <ayat@Zinas-MacBook-Air.local>
2018-07-07 22:32:17 -0400

    got it working. code was ok, but chrome blocked access to local files. remember to run webserver for chrome extension

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 
-function readData(){
-  var file = "World_Cup_Stats_2018.csv";
-  d3.csv(file, function(vals)){
-    console.log(vals);
-  })
-
-}
+var file = "World_Cup_Stats_2018.csv";
+var file1 = "simple.csv";
+d3.csv(file1, function(vals)){
+  console.log(vals[0]);
+})
